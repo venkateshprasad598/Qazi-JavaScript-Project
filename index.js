a2d6754f4e325f9d778f3bf8a277d43e6346a092
Venkatesh Prasad <Venkatesh>
2021-07-09 15:31:52 +0530

    Add them

---
+++ index.js
@@ -0,0 +1,50 @@
+
+// let whatIsYourAgeMan = prompt("WHAT IS YOUR AGE MAN");
+
+
+//  ******************************* Functions *********************************************
+ 
+
+// function greeting() {
+//     let myName = prompt("What is your name?");
+//     let result = "Hello" + myName;
+//     document.getElementById("hey").innerHTML = result;
+//     // document = result;
+//     return document.getElementById("hey").innerHTML;
+// }
+// greeting();
+
+// let sumNumbers = function(num1, num2) {
+//     let result = num1 + 7 + num2;
+//     return result; 
+// }
+// let ok = sumNumbers(3, 10);
+// console.log(ok);
+
+// function greeting(yourName) {
+//     let myName = "Hello " + yourName;
+//     // console.log(myName);
+//     document.getElementById("hey").innerHTML = myName;
+//     return document.getElementById("hey").innerHTML;
+// }
+// let name = prompt("What is your name");
+// let newGreeting = greeting(name);
+// console.log(newGreeting);
+
+
+// ************** Loops **************
+
+let num = 0;
+
+while (num < 100) {
+    num ++;
+    console.log(num);
+}
+
+
+// for(let i = 0; i < 100; i++) {
+//     console.log(i);
+// }
+
+
+// ********** Data Types *******************
